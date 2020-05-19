a14675f5a84cc1d8467fbde378c9d05f52c587f7
palmerotron <julianthewebguy@gmail.com>
2020-05-19 17:32:47 -0400

    Nueva ruta POST y simplificacion de la estructura

--- app/routes.py
+++ app/routes.py
@@ -22,9 +22,38 @@ def reporte():
     )
     cursor = mydb.cursor()
 
-    query = ("Select Usuarios.Nombre, Usuarios.Apellido, Platos.NombrePlato, Lugares.nombreLugar, Revisiones.Puntaje from Revisiones inner join Usuarios  on Revisiones.IdUsuario = Usuarios.IdUsuario inner join Platos on Revisiones.IdPlato = Platos.IdPlato inner join Lugares on Platos.idLugar = Lugares.idLugar ORDER by Revisiones.Puntaje desc")
+    query = ("Select Usuarios.Nombre, Usuarios.Apellido, Platos.NombrePlato, Lugares.nombreLugar, Revisiones.Puntaje, Revisiones.TextoRevision from Revisiones inner join Usuarios  on Revisiones.IdUsuario = Usuarios.IdUsuario inner join Platos on Revisiones.IdPlato = Platos.IdPlato inner join Lugares on Platos.idLugar = Lugares.idLugar ORDER by Revisiones.Puntaje desc")
 
     cursor.execute(query)
 
     rows = cursor.fetchall()
     return jsonify(rows)
+
+@app.route('/post')
+def post():
+    return render_template('form.html')
+
+@app.route('/insertar/', methods=['POST'])
+def insertar():
+    mydb = mysql.connector.connect(
+    host="35.209.8.46",
+    user="abovemed_morfar",
+    db="abovemed_morfar2020",
+    get_warnings=True,
+    connect_timeout=60000,
+    passwd="Watermelon123!"
+    )
+    cursor = mydb.cursor()
+
+    IdPlato = request.form.get('IdPlato')
+    IdUsuario = request.form.get('IdUsuario')
+    Puntaje = request.form.get('Puntaje')
+    Comentario = request.form.get('Texto')
+
+    sql = "INSERT INTO Revisiones (IdPlato, IdUsuario, Puntaje, TextoRevision) VALUES (%s, %s, %s, %s)"
+    val = (IdPlato, IdUsuario, Puntaje, Comentario)
+    cursor.execute(sql, val)
+    mydb.commit()
+
+    print(cursor.rowcount, "record inserted.")
+    return render_template('success.html', title='Success', rows=cursor.rowcount)
